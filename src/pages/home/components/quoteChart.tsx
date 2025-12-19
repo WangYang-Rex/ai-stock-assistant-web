3fee56802be87b955b1d0ef8600444719ee158f2
王洋 <dayang.wy@raycloud.com>
2025-12-19 16:52:27 +0800

    ✨ feat: 优化

--- src/pages/home/components/quoteChart.tsx
+++ src/pages/home/components/quoteChart.tsx
@@ -1,15 +1,85 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { Button, Radio } from 'antd';
+import { Button, Radio, DatePicker } from 'antd';
 import type { Stock } from '@/@types/stock';
-import QuoteTodayk from './quoteTodayk';
-import Quote5Daysk from './quote5Daysk';
 import { quotesApi } from '@/lib/server/quoteApi';
+import dayjs from 'dayjs';
+import type { Quote } from '@/@types/quote';
+import echarts from '@/lib/echartUtil';
+
+const BREAK_GAP = '1%';
+
+const getBreakData = (quoteList: Quote[]) => {
+  const dateArr: string[] = [];
+  // 遍历出 quoteList 所有的日期，去重，并排序
+  quoteList.forEach(item => {
+    if (item.snapshotDate && !dateArr.includes(item.snapshotDate)) {
+      dateArr.push(item.snapshotDate);
+    }
+  });
+  dateArr.sort((a, b) => {
+    return new Date(a).getTime() - new Date(b).getTime();
+  });
+
+  const breakDatas: any = [];
+  dateArr.forEach((it, index) => {
+    if (index > 0) {
+      const beforeDate = dateArr[index - 1];
+      // 插入隔天
+      breakDatas.push({
+        start: new Date(`${beforeDate} 15:00:00`).getTime(),
+        end: new Date(`${it} 09:30:00`).getTime(),
+        gap: BREAK_GAP
+      })
+    }
+    // 插入当天11:30-13:00
+    breakDatas.push({
+      start: new Date(`${it} 11:30:00`).getTime(),
+      end: new Date(`${it} 13:00:00`).getTime(),
+      gap: 0
+    })
+  });
+  return breakDatas;
+}
+
 
 const QuoteChart = (props: { stock: Stock }) => {
   const { stock } = props;
-  const [type, setType] = useState<string>('todayK');
   const [t, setT] = useState<string>(Date.now().toString());
+  const [type, setType] = useState<string>('todayK');
+  const [startTime, setStartTime] = useState<string>(dayjs().format('YYYY-MM-DD 00:00:00'));
+  const [endTime, setEndTime] = useState<string>(dayjs().format('YYYY-MM-DD 23:59:59'));
+  const [quoteList, setQuoteList] = useState<Quote[]>([]);
+  const chartRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    getQuoteList();
+  }, [stock]);
+
+  /** 获取股票行情 */
+  const getQuoteList = async (p?: { startTime: string, endTime: string }) => {
+    const today = new Date().toISOString().split('T')[0];
+    const params = {
+      code: stock.code,
+      marketCode: stock.marketCode?.toString() || '',
+      "startTime": p?.startTime || startTime, // '2025-11-10 00:00:00', // 
+      "endTime": p?.endTime || endTime, // '2025-11-10 23:59:59', //
+      "page": 1,
+      "limit": 10000
+    }
+    const res = await quotesApi.list(params);
+    let newQuoteList = res.quotes;
+    // 过滤出 09:30之后的 和 15:00之前的
+    newQuoteList = newQuoteList.filter(it => {
+      const todayStr = dayjs(it.snapshotTime).format('YYYY-MM-DD');
+      return new Date(it.snapshotTime).getTime() >= new Date(`${todayStr} 09:30:00`).getTime() && new Date(it.snapshotTime).getTime() <= new Date(`${todayStr} 15:00:00`).getTime();
+    });
+    newQuoteList.sort((a, b) => {
+      return new Date(a.snapshotTime || '').getTime() - new Date(b.snapshotTime || '').getTime();
+    });
+    setQuoteList(newQuoteList);
+  }
+
+  /** 同步当天股票行情 */
   const onRefresh = async () => {
     const res = await quotesApi.syncStockQuotesFromAPI({
       code: stock.code,
@@ -20,15 +90,146 @@ const QuoteChart = (props: { stock: Stock }) => {
     }
   }
 
+  useLayoutEffect(() => {
+    chartInit()
+  }, [quoteList]);
+
+  /** 初始化图表 */
+  const chartInit = async () => {
+
+    if (!chartRef.current) {
+      return;
+    }
+    // const _data = generateData();
+
+    const breakData = getBreakData(quoteList);
+    const seriesData = quoteList.map(it => {
+      return [new Date(it.snapshotTime).getTime(), it.latestPrice]
+    });
+    // const formatTime = echarts.time.format;
+    // const breakData = getBreakData(quoteList);
+    // const seriesData = quoteList.filter(it => !!it.snapshotTime).map(it => {
+    //   return [new Date(it.snapshotTime || '').getTime(), it.latestPrice]
+    // });
+    const option = {
+      // Choose axis ticks based on UTC time.
+      // useUTC: true,
+      // title: {
+      //   text: 'Intraday Chart with Breaks (Single Day)',
+      //   left: 'center'
+      // },
+      tooltip: {
+        show: true,
+        trigger: 'axis'
+      },
+      // 网格配置 - 调整图表边距
+      grid: {
+        left: '3%',
+        right: '4%',
+        // bottom: '15%',
+        top: '10%',
+        containLabel: true,
+        show: true,
+        borderColor: 'rgba(24, 144, 255, 0.1)'
+      },
+      xAxis: [
+        {
+          type: 'time',
+          interval: 1000 * 60 * 30,
+          axisLabel: {
+            showMinLabel: true,
+            showMaxLabel: true,
+            formatter: (value, index, extra) => {
+              // if (extra.break.type === 'start') {
+
+              //   return dayjs(extra.break.start,).format('HH:mm') + '/' + dayjs(extra.break.end).format('HH:mm');
+              // }
+              if (extra.break) {
+                // The third parameter is `useUTC: true`.
+                return dayjs(value).format('HH:mm') + '\n' + dayjs(value).format('MM-DD');
+                // return formatTime(value, '{HH}:{mm}\n{weak|{dd}d}', true);
+              }
+              return dayjs(value).format('HH:mm');
+              // return formatTime(value, '{HH}:{mm}', true);
+            },
+            rich: {
+              weak: {
+                color: '#999'
+              }
+            }
+          },
+          breaks: breakData, // _data.breaks,
+          breakArea: {
+            expandOnClick: false,
+            zigzagAmplitude: 0,
+            zigzagZ: 200,
+            itemStyle: {
+              borderColor: 'none',
+              opacity: 0
+            }
+          }
+        }
+      ],
+      yAxis: {
+        type: 'value',
+        min: 'dataMin'
+      },
+      // dataZoom: [
+      //   {
+      //     type: 'inside',
+      //     xAxisIndex: 0
+      //   },
+      //   {
+      //     type: 'slider',
+      //     xAxisIndex: 0
+      //   }
+      // ],
+      series: [
+        {
+          type: 'line',
+          symbolSize: 0,
+          data: seriesData, // _data.seriesData,
+        }
+      ]
+    };
+
+    const myChart = echarts.init(chartRef.current);
+    myChart.setOption(option);
+  }
+
   return (
     <div className="stock-content-chart" key={t}>
       <div className="mb_12 t-FBH">
+        <DatePicker.RangePicker
+          className="mr_12"
+          value={[dayjs(startTime), dayjs(endTime)]}
+          onChange={(value) => {
+            if (value && value[0] && value[1]) {
+              setStartTime(value[0].format('YYYY-MM-DD 00:00:00'));
+              setEndTime(value[1].format('YYYY-MM-DD 23:59:59'));
+              getQuoteList({ startTime: value[0].format('YYYY-MM-DD 00:00:00'), endTime: value[1].format('YYYY-MM-DD 23:59:59') });
+            }
+          }}
+        />
         <Radio.Group
           style={{ width: '300px' }}
           block
           optionType="button"
           value={type}
-          onChange={(e) => setType(e.target.value)}
+          onChange={(e) => {
+            console.log(e.target.value);
+            let _startTime = '2025-10-01 00:00:00'; // 2025-10-01 之后才有数据
+            const _endTime = dayjs().format('YYYY-MM-DD 23:59:59'); // 今天
+            if (e.target.value === 'todayK') {
+              _startTime = dayjs().format('YYYY-MM-DD 00:00:00');
+            } else if (e.target.value === 'fiveDaysK') {
+              _startTime = dayjs().subtract(7, 'day').format('YYYY-MM-DD 00:00:00');
+            }
+            setStartTime(_startTime);
+            setEndTime(_endTime);
+            setType(e.target.value);
+            getQuoteList({ startTime: _startTime, endTime: _endTime });
+          }}
           options={[
             { label: '今日', value: 'todayK' },
             { label: '5日', value: 'fiveDaysK' },
@@ -40,12 +241,7 @@ const QuoteChart = (props: { stock: Stock }) => {
           onRefresh();
         }}>刷新</Button>
       </div>
-      {type === 'todayK' && (
-        <QuoteTodayk stock={stock} />
-      )}
-      {type === 'fiveDaysK' && (
-        <Quote5Daysk stock={stock} />
-      )}
+      <div ref={chartRef} className="ai-report-card-chart-container" ></div>
     </div>
   )
 }
--- src/pages/home/components/stockTable.tsx
+++ src/pages/home/components/stockTable.tsx
@@ -14,27 +14,27 @@ interface DataType {
 }
 
 /** 获取持仓总市值 */
-const getHoldingAmount = (record: Stock) => {
-  const { holdingQuantity = 0, holdingCost = 0, latestPrice = 0 } = record;
-  return latestPrice * holdingQuantity;
-}
+// const getHoldingAmount = (record: Stock) => {
+//   const { holdingQuantity = 0, holdingCost = 0, latestPrice = 0 } = record;
+//   return latestPrice * holdingQuantity;
+// }
 /** 持仓总成本 */
-const getHoldingCost = (record: Stock) => {
-  const { holdingQuantity = 0, holdingCost = 0, latestPrice = 0 } = record;
-  return holdingCost * holdingQuantity;
-}
+// const getHoldingCost = (record: Stock) => {
+//   const { holdingQuantity = 0, holdingCost = 0, latestPrice = 0 } = record;
+//   return holdingCost * holdingQuantity;
+// }
 /** 持仓浮盈/亏损 */
-const getHoldingChangeAmount = (record: Stock) => {
-  const holdingAmount = getHoldingAmount(record);
-  const holdingCost = getHoldingCost(record);
-  return holdingAmount - holdingCost;
-}
+// const getHoldingChangeAmount = (record: Stock) => {
+//   const holdingAmount = getHoldingAmount(record);
+//   const holdingCost = getHoldingCost(record);
+//   return holdingAmount - holdingCost;
+// }
 /** 持仓浮盈/亏损百分比 */
-const getHoldingChangePercent = (record: Stock) => {
-  const holdingChangeAmount = getHoldingChangeAmount(record);
-  const holdingCost = getHoldingCost(record);
-  return holdingChangeAmount / holdingCost;
-}
+// const getHoldingChangePercent = (record: Stock) => {
+//   const holdingChangeAmount = getHoldingChangeAmount(record);
+//   const holdingCost = getHoldingCost(record);
+//   return holdingChangeAmount / holdingCost;
+// }
 
 /** 股票表格 */
 const StockTable = () => {
@@ -60,14 +60,14 @@ const StockTable = () => {
   }
 
   /** 更新持仓 */
-  const updateHolding = async (record: Stock) => {
-    const res = await stockApi.updateHolding({
-      code: record.code,
-      holdingQuantity: 15500,
-      holdingCost: 1.339,
-    });
-    getStockList();
-  }
+  // const updateHolding = async (record: Stock) => {
+  //   const res = await stockApi.updateHolding({
+  //     code: record.code,
+  //     holdingQuantity: 15500,
+  //     holdingCost: 1.339,
+  //   });
+  //   getStockList();
+  // }
 
   const columns: TableProps['columns'] =[
     {
@@ -82,44 +82,63 @@ const StockTable = () => {
       title: '最新价',
       dataIndex: 'latestPrice',
       key: 'latestPrice',
+      render: (value: any, record: any, index: number) => {
+        let textColor = '';
+        if (record.latestPrice) {
+          if (record.latestPrice > 0) {
+            textColor = 'text-color-red'
+          } else {
+            textColor = 'text-color-green'
+          }
+        }
+        return <a className={textColor}>{parseFloat(record.latestPrice || 0).toFixed(3)}</a>
+      },
     },
     {
       title: '今日涨跌幅',
       dataIndex: 'changePercent',
       key: 'changePercent',
       render: (value: any, record: any, index: number) => {
-        return <a>{record.changePercent}%</a>
+        let textColor = '';
+        if (record.changePercent) {
+          if (record.changePercent > 0) {
+            textColor = 'text-color-red'
+           } else {
+            textColor = 'text-color-green'
+          }
+        }
+        return <a className={textColor}>{parseFloat(record.changePercent || 0).toFixed(3)}%</a>
       },
     },
-    {
-      title: '持仓盈亏',
-      dataIndex: 'holdingChangeAmount',
-      key: 'holdingChangeAmount',
-      render: (value: any, record: any, index: number) => {
-        const holdingChangeAmount = getHoldingChangeAmount(record);
-        const holdingChangePercent = getHoldingChangePercent(record);
-        return <a>{holdingChangeAmount}/{formatPercent(holdingChangePercent)}</a>
-      },
-    },
-    {
-      title: '持仓数量/市值',
-      dataIndex: 'holdingQuantity',
-      key: 'holdingQuantity',
-      render: (value: any, record: any, index: number) => {
-        const holdingAmount = getHoldingAmount(record);
-        return <a>{record.holdingQuantity}/{holdingAmount}</a>
-      },
-    },
-    {
-      title: '成本/现价',
-      dataIndex: 'holdingCost',
-      key: 'holdingCost',
-      render: (value: any, record: any, index: number) => {
-        return (
-          <a>{record.holdingCost}/{record.latestPrice}</a>
-        )
-      },
-    },
+    // {
+    //   title: '持仓盈亏',
+    //   dataIndex: 'holdingChangeAmount',
+    //   key: 'holdingChangeAmount',
+    //   render: (value: any, record: any, index: number) => {
+    //     const holdingChangeAmount = getHoldingChangeAmount(record);
+    //     const holdingChangePercent = getHoldingChangePercent(record);
+    //     return <a>{holdingChangeAmount}/{formatPercent(holdingChangePercent)}</a>
+    //   },
+    // },
+    // {
+    //   title: '持仓数量/市值',
+    //   dataIndex: 'holdingQuantity',
+    //   key: 'holdingQuantity',
+    //   render: (value: any, record: any, index: number) => {
+    //     const holdingAmount = getHoldingAmount(record);
+    //     return <a>{record.holdingQuantity}/{holdingAmount}</a>
+    //   },
+    // },
+    // {
+    //   title: '成本/现价',
+    //   dataIndex: 'holdingCost',
+    //   key: 'holdingCost',
+    //   render: (value: any, record: any, index: number) => {
+    //     return (
+    //       <a>{record.holdingCost}/{record.latestPrice}</a>
+    //     )
+    //   },
+    // },
     {
       title: '操作',
       dataIndex: 'action',
@@ -128,7 +147,7 @@ const StockTable = () => {
         return (
           <Space size="middle">
             <a onClick={ ()=>{ updateStockData(record) }}>更新数据</a>
-            <a onClick={ ()=>{ updateHolding(record) }}>更新持仓</a>
+            {/* <a onClick={ ()=>{ updateHolding(record) }}>更新持仓</a> */}
           </Space>
         )
       },
